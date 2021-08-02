280c7cc07bed26c9d7b0ff62eea175cee9463de8
Akash <akash.akash@ihsmarkit.com>
2021-08-02 11:14:49 +0530

    Module in python | import

---
+++ module_import.py
@@ -0,0 +1,11 @@
+"""
+    import myfile -- or any python file to make each function of myfile available
+    myfile.my_function()
+
+    import myfile as mf -- it will import the file i.e module and give a name mf instead of default as module or file name
+    mf.my_function()
+
+    from myfile import my_function,my_var -- this will import only function, variable which are provided comma seperated
+    my_function() -- can call function directly without module name
+
+"""
